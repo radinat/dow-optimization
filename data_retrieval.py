84118f10fcc34fba223f826cde94f3497ae39845
Radina Talanova <32281618+radinat@users.noreply.github.com>
2021-05-22 14:54:28 +0300

    Update data_retrieval.py

--- data_retrieval.py
+++ data_retrieval.py
@@ -11,27 +11,29 @@ from pandas_datareader import data as wb  #module has to be installed
 #data = pd.read_csv("assets_data.csv", index_col=0)
 # Import historical data
 
-symbols = ['AXP', 'AMGN', 'AAPL', 'BA','CAT', 'CSCO', 'CVX',
-           'GS','HD','HON','IBM','INTC','JNJ','KO','JPM',
-           'MCD','MMM','MRK','MSFT','NKE','PG','TRV','UNH',
-           'CRM','VZ','V','WBA','WMT','DIS']
-dowSym = ['^DJI']
+symbols = [ 'AAPL','IBM', 'JPM',
+           'MCD','MSFT','NKE', 'GC=F']
+snpSym = ['^GSPC']
 
 noa = len(symbols)
 data = pd.DataFrame()
-dow = pd.DataFrame()
+snp = pd.DataFrame()
 
 for sym in symbols:
   data[sym] = wb.DataReader(sym, data_source='yahoo',
   start='2011-01-01', end='2021-01-01')['Adj Close']
 
-for sym in dowSym:
-  dow[sym] = wb.DataReader(sym, data_source='yahoo',
+for sym in snpSym:
+  snp[sym] = wb.DataReader(sym, data_source='yahoo',
   start='2011-01-01', end='2021-01-01')['Adj Close']
   
 data.isna().sum()
-  
-data.index=data.index.astype('datetime64[ns]')
-dow.index=dow.index.astype('datetime64[ns]')
+data = data.fillna(method='ffill') #fill na with last observation carried forward
 
-#data.to_csv(r'C:\Users\radina\Desktop\MASTERS\Python_for_Finance\assets_data.csv', index = True)
+#rename gold column
+data = data.rename(columns={'GC=F': 'GOLD'})
+#change index types to datetime
+data.index=data.index.astype('datetime64[ns]')
+snp.index=snp.index.astype('datetime64[ns]')
+
+#data.to_csv(r'C:\Users\radina\Desktop\MASTERS\Techniques/assets_data.csv', index = True)
